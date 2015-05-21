36b39f2024941c65276a061815a3c9740d788762
Igor Gnatenko <i.gnatenko.brain@gmail.com>
2015-05-21 19:23:15 +0300

    meson wrap example
    Signed-off-by: Igor Gnatenko <i.gnatenko.brain@gmail.com>

---
+++ main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <enet/enet.h>
+
+int 
+main (int argc, char **argv) 
+{
+  if (enet_initialize () != 0)
+  {
+    fprintf (stderr, "An error occurred while initializing ENet.\n");
+    return EXIT_FAILURE;
+  }
+  atexit (enet_deinitialize);
+}
